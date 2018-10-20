54487730dc6a068f6b6fb4de00135c83bc51a90f
brayandev <anttonybrayann@gmail.com>
2018-10-20 13:11:40 -0300

    Add connection to mongoDB

--- authentication/repository.go
+++ authentication/repository.go
@@ -1,13 +1,40 @@
 package authentication
 
+import (
+	"fmt"
+
+	"github.com/API-AUTENTICATION/config"
+	mgo "gopkg.in/mgo.v2"
+)
+
 // Repository ...
 type Repository interface {
 	Authentication(Login, Password string) error
-	Save(register Register) error
+	Save(register *Register) error
 }
 
-// RegisterDB ...
-type RegisterDB struct {
-	Server   string
-	Database string
+// Session ...
+type Session struct {
+	session *mgo.Session
+}
+
+// NewSession ...
+func NewSession(url string) (*Session, error) {
+	session, err := mgo.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Session{session}, err
+}
+
+// Save ...
+func Save(register *Register) error {
+	config := config.NewConfig()
+	session, err := NewSession(config.MongoDBEndpoint)
+	if err != nil {
+		return err
+	}
+	fmt.Println(session)
+	return nil
 }
